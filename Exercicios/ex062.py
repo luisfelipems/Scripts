04a06bf2e771a4e78acdadf6c54c022344b4b519
Felipe - FPCS <luisfelipe.martinssilva@gmail.com>
2024-11-04 17:04:42 -0300

    Adicioanando os exercícios 61, 62 e 63.

---
+++ Exercicios/ex062.py
@@ -0,0 +1,26 @@
+"""
+Exercício Python 62: 
+Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. 
+O programa encerrará quando ele disser que quer mostrar 0 termos.
+"""
+
+primeiro = int(input("Primeiro termo: "))
+razão = int(input("Razão: "))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+
+while mais != 0:
+    total += mais
+
+    while cont <= total:
+        print("{}".format(termo), end=" → ")
+        termo = termo + razão
+        cont += 1
+
+    print("PAUSA")
+
+    mais = int(input("Quantos termos você quer mostrar a mais? "))
+
+print("Progressão finalizada com {} termos mostrados!!!".format(total))
